d1c28b15e98ffe06a91a11b599af899f2f9f7eaf
vit-ndpm <137673140+vit-ndpm@users.noreply.github.com>
2023-09-03 11:13:24 +0530

    Initial commit

---
+++ app/src/main/java/com/degs/communicationplan/MainActivity.java
@@ -0,0 +1,16 @@
+package com.degs.communicationplan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "hi", Toast.LENGTH_SHORT).show();
+    }
+}
